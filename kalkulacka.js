8c7d0034407b7566bbd0c9c2cd6da53e2f7084c9
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-04-20 10:51:49 +0200

    Add function

---
+++ kalkulacka.js
@@ -0,0 +1,19 @@
+function vypocet(){
+    let cislo1 = document.querySelector("input[name=cislo1]").value;
+    let cislo2 = document.querySelector("input[name=cislo2]").value;
+    
+    let vysledek;
+
+    cislo1=parseInt(cislo1);
+    cislo2=parseInt(cislo2);
+    
+    znamenko = document.querySelector("select.option").value;
+    
+    if (znamenko === "+"){
+    vysledek=cislo1+cislo2;
+    document.querySelector('.vysledek').innerHTML='Vysledek je: ' + vysledek;
+    } else {
+        vysledek=cislo1-cislo2;
+        document.querySelector('.vysledek').innerHTML='Vysledek je: ' + vysledek;
+    }
+}
