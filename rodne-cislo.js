2e7e5d9579f1aa57e717a8eb39ecea345519df36
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-04-21 12:25:15 +0200

    delete comment

--- rodne-cislo.js
+++ rodne-cislo.js
@@ -34,20 +34,3 @@ if (!(mesic>=1 && mesic<=12 || mesic>=51&&mesic<=62)) {
         document.querySelector('.vysledek').innerHTML="Bylo vloženo rodné číslo.";
         }
 }
-
-
-// první dvojčíslí vyjadřuje poslední dvě číslice roku narození,
-//  druhé dvojčíslí vyjadřuje měsíc narození,
-// u žen zvýšené o 50,
-// třetí dvojčíslí vyjadřuje den narození;
-// čtyřmístná koncovka je rozlišujícím znakem fyzických osob 
-// narozených v tomtéž kalendářním dnu.
-// Rodná čísla přidělená fyzickým osobám narozeným před
-// 1. 1. 1954 mají stejnou strukturu, jsou však devítimístná
-//  s třímístnou koncovkou a nesplňují podmínku dělitelnosti
-//  jedenácti.
-
-
-// done
-// - Rodné číslo je desetimístné číslo, které je dělitelné jedenácti
-// beze zbytku
