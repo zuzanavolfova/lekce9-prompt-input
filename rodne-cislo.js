988c9c507bfaaab408486a87bf341f7ae179979b
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-04-21 11:22:12 +0200

    add file JS, add input and button, add global varies

---
+++ rodne-cislo.js
@@ -0,0 +1,40 @@
+
+let rodneCislo= document.querySelector("input[name=rodneCislo]").value;
+let rok=rodneCislo.substr(0,2);
+let mesic=rodneCislo.substr(2,2);
+let den=rodneCislo.substr(4,2);
+
+console.log(rok);
+console.log(mesic);
+console.log(den);
+
+
+function overRC(){
+let rodneCislo= document.querySelector("input[name=rodneCislo]").value;
+
+if 
+
+
+// (rodneCislo % 11 !== 0) {
+//         document.querySelector('.vysledek').innerHTML="Nebylo vloženo rodné číslo.";
+//     } else if ( {
+//     } else {
+//         document.querySelector('.vysledek').innerHTML="Bylo vloženo rodné číslo.";
+//     }
+// }
+
+// první dvojčíslí vyjadřuje poslední dvě číslice roku narození,
+//  druhé dvojčíslí vyjadřuje měsíc narození,
+// u žen zvýšené o 50,
+// třetí dvojčíslí vyjadřuje den narození;
+// čtyřmístná koncovka je rozlišujícím znakem fyzických osob 
+// narozených v tomtéž kalendářním dnu.
+// Rodná čísla přidělená fyzickým osobám narozeným před
+// 1. 1. 1954 mají stejnou strukturu, jsou však devítimístná
+//  s třímístnou koncovkou a nesplňují podmínku dělitelnosti
+//  jedenácti.
+
+
+// done
+// - Rodné číslo je desetimístné číslo, které je dělitelné jedenácti
+// beze zbytku;
